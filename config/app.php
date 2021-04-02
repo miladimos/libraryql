d0ca23819cfb40be95c14fae40794d6eda397639
milad.jafari6210@gmail.com <milad.jafari6210@gmail.com>
2021-04-03 04:26:15 +0430

    install rebing/graphql-laravel package for qraphql

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Rebing\GraphQL\GraphQLServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -227,6 +228,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        // Packages
+        'GraphQL' => Rebing\GraphQL\Support\Facades\GraphQL::class,
 
     ],
 
